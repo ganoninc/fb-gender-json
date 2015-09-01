7093b1af9cd2749ebe5fe5dcfcef655e6fce8198
Romain Giovanetti <ganoninc@gmail.com>
2015-09-01 15:26:07 +0200

    Added a script to build the list according the the original data, the original data and the new list. Updated README

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # fb-gender-json
 A Facebook generated name list with their gender in a JSON file
 
-The aim of this list is to be used for social network analysis. 
-The JSON format makes it easier to use. 
+The aim of this list is to be used for social network analysis.
+The JSON format makes it easier to use.
+
+The list was built using a python script (see the build directory).
+The original list is in the original_data directory.
 
 Data from [
 What's in a Name: A Study of Names, Gender Inference, and Gender Behavior in Facebook](https://sites.google.com/site/facebooknamelist/home)
---
+++ build/make.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+#	Adapt the data from the original list into a JSON
+
+import sys
+import codecs
+import json
+
+most_likely_ratio = 1.5
+
+def adapt():
+	firstnames = {}
+	f = codecs.open('../original_data/firstname.csv','r',encoding='utf8')
+	for line in f:
+		line_elts = line.split(",")
+		most_likely = "unisex"
+		if float(line_elts[1]) >= most_likely_ratio * float(line_elts[2]):
+			most_likely = "male"
+		elif float(line_elts[2]) >= most_likely_ratio * float(line_elts[1]):
+			most_likely = "female"
+		firstnames[line_elts[0].title()] = {
+			"male":line_elts[1],
+			"female":line_elts[2],
+			"most_likely": most_likely
+		}
+	f.close()
+	with open('../fb-gender.json', 'w') as outfile:
+		json.dump(firstnames, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
+if __name__ == '__main__':
+    try:
+        adapt()
+    except KeyboardInterrupt:
+        print >> sys.stderr, '\nExiting by user request.\n'
+        sys.exit(0)
